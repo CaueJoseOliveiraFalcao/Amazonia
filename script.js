320232cd265de75d91b9312af119c82bb5c822b5
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-08-25 12:16:22 -0300

    navbar

---
+++ script.js
@@ -0,0 +1,9 @@
+const hambur = document.querySelector('hambug')
+
+
+function play(){
+    document.querySelector('.mobile').style.marginTop = '-8vh'
+}
+function exit(){
+    document.querySelector('.mobile').style.marginTop = '-110vh'
+}
